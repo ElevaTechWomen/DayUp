ec7965a38dc904b2f5e4f7c2591add41fb1b29b4
GisB <kgiselle17@gmail.com>
2026-03-20 15:57:38 -0500

    chore: configure pre-commit hook with Husky

--- eslint.config.js
+++ eslint.config.js
@@ -1,10 +1,26 @@
 // https://docs.expo.dev/guides/using-eslint/
 const { defineConfig } = require('eslint/config');
 const expoConfig = require('eslint-config-expo/flat');
+const tsPlugin = require('@typescript-eslint/eslint-plugin');
 
 module.exports = defineConfig([
   expoConfig,
   {
     ignores: ['dist/*'],
+  },
+  {
+    plugins: {
+      '@typescript-eslint': tsPlugin,
+    },
+    rules: {
+      indent: ['error', 2],
+      'react/jsx-indent': ['error', 2],
+      quotes: ['error', 'single', { avoidEscape: true }],
+      semi: ['error', 'always'],
+      'no-trailing-spaces': ['error'],
+      'eol-last': ['error', 'always'],
+      'no-console': ['warn'],
+      '@typescript-eslint/no-unused-vars': ['error'],
+    },
   },
 ]);
